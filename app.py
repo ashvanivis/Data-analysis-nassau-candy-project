40ec9801bc20348e01a452ebdbd7162ac9cc0903
Ashvani Vishwakarma <ashvanivishwakarma4@gmail.com>
2026-05-23 14:30:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ product = st.sidebar.text_input(
 # ---------------- LOAD DATA ---------------- #
 
 df = pd.read_csv(
-    r"C:\Users\hp\Videos\project\nassau_candy.csv"
+    r"nassau_candy.csv"
 )
 
 # ---------------- DATE CONVERSION ---------------- #
